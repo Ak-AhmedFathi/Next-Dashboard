008ff60ee3aa005b29c3a92480d4c8cef6994024
Ahmed-Fathi <ahmedfathi1792@yahoo.com>
2025-08-06 11:10:07 +0200

    changing layout

---
+++ src/app/(dashboard)/list/attendance/page.tsx
@@ -0,0 +1,9 @@
+const Attendance = () => {
+  return (
+    <div className="flex items-center justify-center h-screen text-2xl font-bold">
+      Coming Soon
+    </div>
+  )
+}
+
+export default Attendance;
---
+++ src/app/(dashboard)/list/messages/page.tsx
@@ -0,0 +1,9 @@
+const Messages = () => {
+  return (
+    <div className="flex items-center justify-center h-screen text-2xl font-bold">
+      Coming Soon
+    </div>
+  )
+}
+
+export default Messages;
